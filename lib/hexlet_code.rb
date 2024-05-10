e94831767ade9825a35259e17b2857c78316ea3d
Pletennyy2 <pletennyy@yandex.ru>
2024-05-10 12:24:07 +0300

    4th

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -7,12 +7,6 @@ module HexletCode
   autoload(:FormRender, 'hexlet_code/form_render')
   autoload(:Tag, 'hexlet_code/tag')
 
-  def self.send(_entity, attributes = {})
-    builded_form = FormBuilder.new(_entity, **attributes)
-    yield(builded_form) if block_given?
-    FormRender.render_html(builded_form.form_body)
-  end
-
   def self.form_for(entity, attributes = {})
     builded_form = FormBuilder.new(entity, **attributes)
     yield(builded_form) if block_given?
