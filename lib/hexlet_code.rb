e5460407abf58cd5d669ef89f1fc5589f60d3b19
Pletennyy2 <pletennyy@yandex.ru>
2024-05-10 13:08:48 +0300

    4th

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -7,15 +7,17 @@ module HexletCode
   autoload(:FormRender, 'hexlet_code/form_render')
   autoload(:Tag, 'hexlet_code/tag')
 
-  def self.send_form(_entity, attributes = {})
-    builded_form = FormBuilder.new(_entity, **attributes)
-    yield(builded_form) if block_given?
-    FormRender.render_html(builded_form.form_body)
-  end
-
   def self.form_for(entity, attributes = {})
     builded_form = FormBuilder.new(entity, **attributes)
     yield(builded_form) if block_given?
     FormRender.render_html(builded_form.form_body)
+  end
+
+  def self.method_missing(method_name, *_args, &)
+    raise NoMethodError, "undefined method `#{method_name}` for #{self}"
+  end
+
+  def self.respond_to_missing?(method_name, include_private = false)
+    method_name.to_s.start_with?('send', 'form_for') || super
   end
 end
